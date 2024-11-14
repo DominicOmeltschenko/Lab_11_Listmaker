d10d52d4d612ae321805acbd9204eef56baf67a0
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-11-14 15:40:25 -0500

    final commit, everything is mostly working as intended

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,25 @@ public class Main {
 
 
             do {
+                display();
                 currentChoice = SafeInput.getRegExString(pipe, "What would you like to do? A (add item), D (delete item), I (insert an item), P (print your list), Q (quit the program) ", "[AaDdIiPpQq]");
-                if (currentChoice.equalsIgnoreCase("a"))
+                switch (currentChoice.toUpperCase())
                 {
-                    addItem();
+                    case "A":
+                        addItem();
+                        break;
+                    case "D":
+                        deleteItem();
+                        break;
+                    case "I":
+                        insertItem();
+                        break;
+                    case "P":
+                        display();
+                        break;
                 }
-                else if (currentChoice.equalsIgnoreCase("d"))
-                {
-                    deleteItem();
-                }
-                else if (currentChoice.equalsIgnoreCase("i"))
-                {
-                    insertItem();
-                }
-                else if (currentChoice.equalsIgnoreCase("p"))
-                {
-                    display();
-                }
+
+
 
             } while (!currentChoice.equalsIgnoreCase("q"));
 
@@ -40,6 +42,7 @@ public class Main {
         }
     }
     public static void addItem() {
+
         arrList.add(SafeInput.getNonZeroLenString(pipe,"What would you like to add to your list"));
     }
     public static void deleteItem()
@@ -48,6 +51,7 @@ public class Main {
         arrList.remove((SafeInput.getRangedInt(pipe,"Which item would you like to remove from your list" + "[ " + 1 + " - " + arrList.size() + " ]" , 1 , arrList.size() )) - 1);
     }
     public static void insertItem() {
+
         arrList.add((SafeInput.getRangedInt(pipe,"Where would you like to insert your item" + "[ " + 1 + " - " + arrList.size() + " ]" , 1, arrList.size()) - 1), SafeInput.getNonZeroLenString(pipe,"What would you like to add to your list"));
     }
 }
